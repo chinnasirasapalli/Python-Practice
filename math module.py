6f1e26328858e699668ddfc7510a67397c362cc9
chinna sirasapalli <chinna9908@gmail.com>
2026-09-17 19:54:58 +0530

    Add files via upload

---
+++ math module.py
@@ -0,0 +1,11 @@
+import math
+
+number = float(input("enter a number: "))
+
+square_root = math.sqrt(number)
+logrithm = math.log(number)
+sine = math.sin(number)
+
+print("squre root:", square_root)
+print("logrithm:", logrithm)
+print("sine:", sine)
